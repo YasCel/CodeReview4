3c1d5e34908324b71184895442a8644bd91f0d4c
Yasemin Celik <yasemincelik_7@hotmail.com>
2015-10-15 22:03:33 -0700

    Add branching to scrpts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   var number = prompt("Please enter a number");
-    for (var index=1; index <= number; index +=1)
+    for (var index=1; index <= number; index +=1) {
+        if (index % 3 === 0 && index % 5 === 0) {
+          document.write("ping-pong");
+        } else if (index % 3 === 0) {
+          document.write("ping");
+        } else if (index % 5 === 0) {
+          document.write("pong");
+        } else {
+          document.write(index)
+        }
 
 
-
+    }
 });
