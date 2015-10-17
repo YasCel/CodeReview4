bd8e70611b9bd2c6402826ddb4527728b9e37b89
Yasemin Celik <yasemincelik_7@hotmail.com>
2015-10-16 22:13:30 -0700

    add listing functionality to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
   var number = prompt("Please enter a number");
     for (var index=1; index <= number; index +=1) {
-      document.write("<li>");
         if (index % 3 === 0 && index % 5 === 0) {
-          document.write("ping-pong");
+          document.write('<li>'+"ping-pong"+"</li>");
         } else if (index % 3 === 0) {
           document.write("ping");
         } else if (index % 5 === 0) {
           document.write("pong");
         } else {
           document.write(index);
-        }
+        };
 
     }
 });
